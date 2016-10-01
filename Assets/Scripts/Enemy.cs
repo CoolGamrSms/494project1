38a53c59f50b95b94832fc5697bc655984da42b5
Shane Schulte <schulte.shane@gmail.com>
2016-10-01 00:03:59 -0400

    Revised enemies class, populated 2 types of enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,52 +4,148 @@ using System.Collections;
 public abstract class Enemy : MonoBehaviour {
 
     public Sprite[] animation;
+    public int health = 1;
     float animation_start_time;
-    public int fps;
-    public Vector3 forward;
+    protected int fps;
 
-    public float walking_velocity;
-    int cooldown;
-    public int health = 100;
-    public float knockback = 0f;
     Color c;
 
-	public static Enemy instance;
+    bool isHurt;
+    bool isKnockback;
+    bool stopped;
+
+    float roomX, roomY;
 
 	// Use this for initialization
 	public void Start () {
-		instance = this;
+        //Initialize values
         animation_start_time = Time.time;
         c = GetComponent<SpriteRenderer>().material.color;
+        isHurt = false;
+        isKnockback = false;
+        stopped = false;
+
+        //Determine what room boundaries are
+        roomX = Mathf.Floor((transform.position.x - 2) / 16f) * 16f + 2f;
+        roomY = Mathf.Floor((transform.position.y - 2) / 11f) * 11f + 2f;
+        Debug.Log(roomX);
+        Debug.Log(roomY);
+
+        if (isOnScreen()) StartMovement();
+        else stopped = true;
 	}
 
-    public void knockbackEnd()
+    public abstract void StartMovement();
+
+    public abstract void UpdateMovement();
+
+    public void SnapGrid()
     {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Round(pos.x);
+        pos.y = Mathf.Round(pos.y);
+        transform.position = pos;
+    }
+
+    public void Hurt()
+    {
+        if (isHurt) return;
+        isHurt = true;
+        GetComponent<SpriteRenderer>().material.color = Color.red;
+        health--;
+        if (health <= 0) Kill();
+        else Invoke("EndHurt", 0.5f);
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        direction /= direction.sqrMagnitude;
+        if (isKnockback) return;
+        isKnockback = true;
+        GetComponent<Rigidbody>().velocity = direction*10;
+        Invoke("EndKnockback", 0.3f);
+    }
+
+    public void EndKnockback()
+    {
+        if (!isKnockback) return;
+        isKnockback = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        StartMovement();
+    }
+
+    public void EndHurt()
+    {
+        if (!isHurt) return;
+        isHurt = false;
         GetComponent<SpriteRenderer>().material.color = c;
     }
-	
-	// Update is called once per frame
-	public void Update () {
-        if(knockback > 0f)
+
+    public bool isOnScreen()
+    {
+        if (RoomTransition.S.current_state == EntityState.TRANSITION) return false;
+        Camera cam = RoomTransition.S.GetComponent<Camera>();
+        Vector3 p = cam.transform.position;
+        if (p.x > roomX && p.x < roomX + 8 && p.y > roomY && p.y < roomY + 6) return true;
+        return false;
+    }
+
+    public virtual bool CheckCollision()
+    {
+        if (transform.position.x < roomX) return true;
+        if (transform.position.y < roomY) return true;
+        if (transform.position.x > roomX + 11) return true;
+        if (transform.position.y > roomY + 6) return true;
+        return false;
+    }
+
+    public void SnapBounds()
+    {
+        Vector3 p = transform.position;
+        if (p.x < roomX) p.x = roomX;
+        if (p.y < roomY) p.y = roomY;
+        if (p.x > roomX + 11) p.x = roomX + 11;
+        if (p.y > roomY + 6) p.y = roomY + 6;
+        transform.position = p;
+    }
+
+    // Update is called once per frame
+    public void Update () {
+
+        if(!stopped)
         {
-            knockback -= Time.deltaTime / (1.0f / Application.targetFrameRate);
-
-            RaycastHit hit2;
-
-            Debug.DrawRay(transform.position, forward * 2, Color.green);
-            if (Physics.Raycast(transform.position, forward * 2, out hit2))
+            if (!isOnScreen())
             {
-                if (hit2.collider.gameObject.CompareTag("Map"))
-                {
-                    if (hit2.distance < 0.5f) { GetComponent<Rigidbody>().velocity = Vector3.zero; }
-                }
+                stopped = true;
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                SnapGrid();
+                return;
             }
+        }
 
-            if (knockback <= 0f) knockbackEnd();
+        if(stopped)
+        {
+            if (isOnScreen())
+            {
+                stopped = false;
+                StartMovement();
+            }
             return;
         }
+
+        if (isKnockback) {
+            if (CheckCollision()) EndKnockback();
+        } else UpdateMovement();
+
         //Animation
-        int current_frame_index = ((int)((Time.time - animation_start_time) / (1.0 / fps)) % animation.Length);
+        int current_frame_index;
+        if (fps == 0) current_frame_index = 0;
+        else current_frame_index = ((int)((Time.time - animation_start_time) / (1.0 / fps)) % animation.Length);
         GetComponent<SpriteRenderer>().sprite = animation[current_frame_index];
     }
 
@@ -57,39 +153,10 @@ public abstract class Enemy : MonoBehaviour {
 
     public void OnTriggerEnter(Collider coll)
     {
-        if (knockback > 0f) return;
 		if(coll.gameObject.CompareTag("Sword") || coll.gameObject.CompareTag("Explosion") || coll.gameObject.CompareTag("Boomerang"))
         {
-            health--;
-            if(health <= 0)
-            {
-                Destroy(gameObject);
-            }
-            else //Knockback
-            {
-                //Snap to grid
-                Vector3 pos = transform.position;
-                pos.x = Mathf.Round(pos.x);
-                pos.y = Mathf.Round(pos.y);
-                transform.position = pos;
-
-                //Set velocity
-                if(PlayerControl.instance.current_direction == Direction.EAST) forward = new Vector3(1f, 0f);
-                if (PlayerControl.instance.current_direction == Direction.WEST) forward = new Vector3(-1f, 0f);
-                if (PlayerControl.instance.current_direction == Direction.SOUTH) forward = new Vector3(0f, -1f);
-                if (PlayerControl.instance.current_direction == Direction.NORTH) forward = new Vector3(0f, 1f);
-                this.GetComponent<Rigidbody>().velocity = forward * 10f;
-
-                //Set timer
-                knockback = 15f;
-
-                //Set the color
-                
-                GetComponent<SpriteRenderer>().material.color = Color.red;
-
-                //Change tag
-                this.gameObject.tag = "EnemyHurt";
-            }
+            Hurt();
+            Knockback(Vector3.left);
         }
     }
 
